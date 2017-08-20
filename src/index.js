b3c0c3ac0399c0aa8b9458a6e80f3d779667f19d
ankur19379@gmail.com <ankur19379@gmail.com>
2017-08-20 12:14:58 -0400

    added pure redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import reducers from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+console.log(store.getState())
+
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 registerServiceWorker();
